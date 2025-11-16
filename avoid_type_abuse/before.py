0ed778d052e832c20a3b6202dfe3dea96dbeb027
elenachau <elenaxcha@gmail.com>
2025-11-16 12:47:26 -0800

    create before case where role is too broad

---
+++ avoid_type_abuse/before.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    first_name: str
+    last_name: str
+    role: str # role is constrained value, use enum instead; avoid broad types
+
+
+def main():
+    lucy = User(first_name="Lucy", last_name="Smith", role="admin")
+    tim = User(first_name="Tim", last_name="Green", role="manager")
+    print(lucy)
+    print(tim)
+
+
+if __name__ == "__main__":
+    main()
